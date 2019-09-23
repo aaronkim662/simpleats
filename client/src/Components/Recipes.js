1adb349a505ad70102fef2cb27d02664bd886de6
Tommy Liang <tommyliangsays@gmail.com>
2019-09-23 13:29:33 -0400

    updated skeleton files

---
+++ client/src/Components/Recipes.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../css/Recipes.css"
+
+const Recipes = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Recipes
---
+++ client/src/Components/main.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../css/main.css"
+
+const Main = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Main
