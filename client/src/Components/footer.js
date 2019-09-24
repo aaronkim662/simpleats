4e3297fc7870f182d1cd0755926a059436ebf2f7
Tommy Liang <tommyliangsays@gmail.com>
2019-09-24 11:47:47 -0400

    updated thisweek

--- client/src/Components/footer.js
+++ client/src/Components/footer.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "../css/footer.css";
+import FB from "../images/fb.png"
+import Twitter from "../images/twitter.png"
+import Insta from "../images/insta.png"
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="contact">Contact Us: </div>
       <div className="socialMedia">
-        <img className="socialImage" src="https://instagram-brand.com/wp-content/uploads/2016/11/Instagram_AppIcon_Aug2017.png?w=300" alt="instagram"/>
-        <img className="socialImage" src="https://static01.nyt.com/images/2014/08/10/magazine/10wmt/10wmt-articleLarge-v4.jpg?quality=75&auto=webp&disable=upscale0" alt="twitter"/>
-        <img className="socialImage" src="https://image.flaticon.com/icons/png/512/124/124010.png" alt="facebook"/>
+        <img className="socialImage" src={Insta} alt="instagram"/>
+        <img className="socialImage" src={Twitter} alt="twitter"/>
+        <img className="socialImage" src={FB} alt="facebook"/>
       </div>
     </div>
   )
