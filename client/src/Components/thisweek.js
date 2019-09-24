126df7f372b0abd4525768d217f55c77760efeb8
Tommy Liang <tommyliangsays@gmail.com>
2019-09-24 14:29:41 -0400

    cleaning up file errors

--- client/src/Components/thisweek.js
+++ client/src/Components/thisweek.js
@@ -6,13 +6,13 @@ const Thisweek = () => {
   return (
     <div>
       <div className="thisWeekContainer">
-        <img class="circleBase type1 chickenP"></img>
-        <div class="circleBase type1 lobsterR"></div>
-        <div class="circleBase type1 chickenN"></div>
-        <div class="circleBase type1 forthInRow btm"></div>
-        <div class="circleBase type1 btm"></div>
-        <div class="circleBase type1 hidden btm"></div>
-        <div class="circleBase type2">+</div>
+        <div className="circleBase type1 chickenP"></div>
+        <div className="circleBase type1 lobsterR"></div>
+        <div className="circleBase type1 chickenN"></div>
+        <div className="circleBase type1 forthInRow btm"></div>
+        <div className="circleBase type1 btm"></div>
+        <div className="circleBase type1 hidden btm"></div>
+        <div className="circleBase type2">+</div>
       </div>
     </div>
   )
