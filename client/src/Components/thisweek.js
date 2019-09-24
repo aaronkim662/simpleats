699f9236960617401e27e520431c4ee85932d735
Tommy Liang <tommyliangsays@gmail.com>
2019-09-24 10:54:28 -0400

    updating thisweek

--- client/src/Components/thisweek.js
+++ client/src/Components/thisweek.js
@@ -4,13 +4,15 @@ import Progress from './progressbar';
 
 const Thisweek = () => {
   return (
-    <div className="thisWeekContainer">
-      <img class="circleBase type1 chickenP"></img>
-      <div class="circleBase type1 lobsterR"></div>
-      <div class="circleBase type1"></div>
-      <div class="circleBase type1"></div>
-      <div class="circleBase type1"></div>
-      <div class="circleBase type2">+</div>
+    <div><Progress/>
+      <div className="thisWeekContainer">
+        <img class="circleBase type1 chickenP"></img>
+        <div class="circleBase type1 lobsterR"></div>
+        <div class="circleBase type1"></div>
+        <div class="circleBase type1"></div>
+        <div class="circleBase type1"></div>
+        <div class="circleBase type2">+</div>
+      </div>
     </div>
   )
 }
