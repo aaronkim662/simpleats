f04f655abe511a340c1096e3252872afed6a166a
Tommy Liang <tommyliangsays@gmail.com>
2019-09-24 08:47:07 -0400

    updated cards

--- client/src/Components/Recipescard.js
+++ client/src/Components/Recipescard.js
@@ -1,10 +1,21 @@
 import React from "react";
-import "../css/receipes.css";
+import "../css/recipes.css";
+import one from "../images/1.png"
+import two from "../images/2.png"
+import three from "../images/3.png"
+import four from "../images/4.png"
+import five from "../images/5.png"
+import six from "../images/6.png"
 
 const Recipescard = () => {
   return (
     <div>
-      <h1>This is receipes</h1>
+      <img className="recipes" src={one}/>
+      <img className="recipes" src={two}/>
+      <img className="recipes" src={three}/>
+      <img className="recipes" src={four}/>
+      <img className="recipes" src={five}/>
+      <img className="recipes" src={six}/>
       
     </div>
   )
--- client/src/Components/main.js
+++ client/src/Components/main.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "../css/main.css";
 import Progress from './progressbar';
+import Recipescard from './Recipescard'
 
 const Main = () => {
   return (
     <div>
-
       <Progress />
+      < Recipescard />
     </div>
   )
 }
